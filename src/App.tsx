e6ed91f11b43ce7db6757a7d0d782be49aacc63c
coffiejason <coffiejasoncj@gmail.com>
2022-10-15 08:32:17 +0000

    day 1

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+import { Home, Dashboard, Onboarding } from './pages';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
+        <Route path="/onboarding" element={<Onboarding />}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+const Home = () => {
+
+    const authToken = true;
+
+    const handleClick = {};
+
+
+
+  return (
+    <div className="home">
+        <h1>Swipe Right</h1>
+        {/* <Button className="primary-button" onClick={handleClick}>
+            {authToken ? 'Signout':'Create Account'}
+        </Button> */}
+    </div>
+  )
+}
+
+export default Home;
